7f277b193b9e7dc32159a83fa9779466f16742fb
Hoosang Lee <lhs223@gmail.com>
2022-08-30 12:14:55 +0900

    init update

---
+++ launch_docker.sh
@@ -0,0 +1,8 @@
+sudo docker run --gpus all -it --net=host --privileged \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=unix$DISPLAY \
+    -v /home/hmci/Downloads:/root/Downloads \
+    -v `pwd`/share:/root/share \
+    --name ur10_2 \
+    lhs223/hsa_project:ur10 \
+    bash
